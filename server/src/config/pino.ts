c555aa89a303cd0944373c7912dcb0bfd9127896
Linnxw <ulin230606@gmail.com>
2024-01-23 21:35:25 +0700

    type check

--- server/src/config/pino.ts
+++ server/src/config/pino.ts
@@ -1,12 +1,11 @@
 import pino from "pino"
 import pretty from "pino-pretty"
-import moment from "moment"
 
 export const logger=pino({
   base:{
    pid:false
   },
-  timestamp:()=>`,"time":"${moment().format("HH:mm:ss")}"`,
+  timestamp:()=>`,"time":"${new Date().getTime}"`,
 },
   pretty()
 )
